2691ed77fa2da492bddad88b1767ccbe3c5dd17c
Maxim Vancompernolle <mhv6@gatech.edu>
2024-02-01 14:06:07 -0500

    Add hube12/SEED dependencies and clean up project

---
+++ src/main/java/com/mvc/Main.java
@@ -0,0 +1,4 @@
+package com.mvc;
+
+public class Main {
+}
--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.hdwghseedfinding {
-    requires javafx.controls;
-    requires javafx.fxml;
-            requires org.apache.groovy;
-        
-                            
-    opens com.example.hdwghseedfinding to javafx.fxml;
-    exports com.example.hdwghseedfinding;
-}
