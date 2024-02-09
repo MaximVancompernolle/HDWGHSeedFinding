188a52b2c0139b5f4d52e80e773b1e8f4d60c09e
Maxim Vancompernolle <mhv6@gatech.edu>
2024-02-08 19:01:56 -0500

    switch back to random seed searching; add bastion and fortress filter

--- src/main/java/com/mvc/Main.java
+++ src/main/java/com/mvc/Main.java
@@ -2,35 +2,35 @@ package com.mvc;
 
 import com.seedfinding.mccore.rand.ChunkRand;
 
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Generating a seed...");
         ChunkRand chunkRand = new ChunkRand();
+        Random random = new Random();
+        int seedMatches = 0;
 
-        try {
-            Scanner scanner = new Scanner(new File("./12eyeseeds.txt"));
-            FileWriter output = new FileWriter("./city-sword.txt");
+        FileWriter output = new FileWriter("./output.txt");
+        long worldSeed;
 
-            while (scanner.hasNextLong()) {
-                long worldSeed = scanner.nextLong();
-                if (filterStructureSeed(worldSeed % (1L << 48), chunkRand)) {
-                    output.write(worldSeed + "\n");
-                }
+        while (seedMatches < 1_000) {
+            worldSeed = random.nextLong();
+
+            if (filterStructureSeed(worldSeed % (1L << 48), chunkRand)) {
+                output.write(worldSeed + "\n");
+                seedMatches++;
             }
-            output.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
+
+        output.close();
     }
 
     public static boolean filterStructureSeed(long structureSeed, ChunkRand chunkRand) {
         StructureFilter filter = new StructureFilter(structureSeed, chunkRand);
-        return filter.filterEnd();
+        return filter.filterNether() && filter.filterEnd();
     }
 }
--- src/main/java/com/mvc/StructureFilter.java
+++ src/main/java/com/mvc/StructureFilter.java
@@ -1,52 +1,125 @@
 package com.mvc;
 
 import com.seedfinding.mcbiome.source.EndBiomeSource;
+import com.seedfinding.mcbiome.source.NetherBiomeSource;
 import com.seedfinding.mccore.rand.ChunkRand;
 import com.seedfinding.mccore.util.data.Pair;
 import com.seedfinding.mccore.util.math.DistanceMetric;
-import com.seedfinding.mccore.util.pos.BPos;
 import com.seedfinding.mccore.util.pos.CPos;
 import com.seedfinding.mccore.util.pos.RPos;
 import com.seedfinding.mccore.version.MCVersion;
 import com.seedfinding.mcfeature.loot.ChestContent;
 import com.seedfinding.mcfeature.loot.item.Item;
 import com.seedfinding.mcfeature.loot.item.ItemStack;
+import com.seedfinding.mcfeature.structure.BastionRemnant;
 import com.seedfinding.mcfeature.structure.EndCity;
+import com.seedfinding.mcfeature.structure.Fortress;
 import com.seedfinding.mcfeature.structure.generator.structure.EndCityGenerator;
 import com.seedfinding.mcterrain.terrain.EndTerrainGenerator;
+import com.seedfinding.mcterrain.terrain.NetherTerrainGenerator;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.List;
 
 public class StructureFilter {
     public static final MCVersion VERSION = MCVersion.v1_16_1;
-    public static final double MAX_DIST = 128.0D * 128.0D;
-    public EndCity ec = new EndCity(VERSION);
-    public EndCityGenerator ecGen = new EndCityGenerator(VERSION);
+    public static final double MAX_DIST = 12D * 12D;
     private final long structureSeed;
     private final ChunkRand chunkRand;
-    public EndBiomeSource source;
-    public EndTerrainGenerator terrainGen;
+    public BastionRemnant bastion = new BastionRemnant(VERSION);
+    public Fortress fortress = new Fortress(VERSION);
+    public EndCity ec = new EndCity(VERSION);
+    public EndCityGenerator ecGen = new EndCityGenerator(VERSION);
+    public NetherBiomeSource netherBiomeSource;
+    public NetherTerrainGenerator netherTerrainGenerator;
+    public EndBiomeSource endBiomeSource;
+    public EndTerrainGenerator endTerrainGen;
 
     public StructureFilter(long structureSeed, ChunkRand chunkRand) {
         this.structureSeed = structureSeed;
         this.chunkRand = chunkRand;
-        this.source = new EndBiomeSource(VERSION, structureSeed);
-        this.terrainGen = new EndTerrainGenerator(source);
+        this.netherBiomeSource = new NetherBiomeSource(VERSION, structureSeed);
+        this.netherTerrainGenerator = new NetherTerrainGenerator(netherBiomeSource);
+        this.endBiomeSource = new EndBiomeSource(VERSION, structureSeed);
+        this.endTerrainGen = new EndTerrainGenerator(endBiomeSource);
     }
 
-    public static BPos firstGateway(long structureSeed) {
+    public boolean filterNether() {
+        RPos[][] bastionRegions = new RPos[2][2];
+        RPos[][] fortressRegions = new RPos[2][2];
+
+        for (int x = -1; x <= 0; x++) {
+            for (int z = -1; z <= 0; z++) {
+                bastionRegions[x + 1][z + 1] = new RPos(x, z, 27 << 4);
+                fortressRegions[x + 1][z + 1] = new RPos(x, z, 27 << 4);
+            }
+        }
+
+        HashSet<CPos> bastionLocations = new HashSet<>();
+
+        for (RPos[] rowOfBastionRegions : bastionRegions) {
+            for (RPos bastionRegion : rowOfBastionRegions) {
+                CPos bastionLocation = bastion.getInRegion(structureSeed, bastionRegion.getX(), bastionRegion.getZ(), chunkRand);
+
+                if (bastionLocation == null) {
+                    continue;
+                }
+
+                double bastionDistance = bastionLocation.getMagnitudeSq();
+
+                if (bastionDistance < MAX_DIST && filterNetherBiomes(bastionLocation)) {
+                    bastionLocations.add(bastionLocation);
+                }
+            }
+        }
+
+        if (bastionLocations.isEmpty()) {
+            return false;
+        }
+
+        for (RPos[] rowOfFortressRegions : fortressRegions) {
+            for (RPos fortressRegion : rowOfFortressRegions) {
+                CPos fortressLocation = fortress.getInRegion(structureSeed, fortressRegion.getX(), fortressRegion.getZ(), chunkRand);
+
+                if (fortressLocation == null) {
+                    continue;
+                }
+
+                for (CPos bastionEntry : bastionLocations) {
+                    double fortressDistance = fortressLocation.distanceTo(bastionEntry, DistanceMetric.EUCLIDEAN_SQ);
+
+                    if (fortressDistance < MAX_DIST) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public boolean filterNetherBiomes(CPos bastionLocation) {
+        return bastion.canSpawn(bastionLocation, netherBiomeSource) && bastion.canGenerate(bastionLocation, netherTerrainGenerator);
+    }
+
+    public static CPos firstGateway(long structureSeed) {
         ArrayList<Integer> gateways = new ArrayList<>();
-        for (int i = 0; i < 20; i++) gateways.add(i);
+        for (int i = 0; i < 20; i++) {
+            gateways.add(i);
+        }
         Collections.shuffle(gateways, new Random(structureSeed));
         double angle = 2.0 * ((-1 * Math.PI) + (Math.PI / 20) * (gateways.remove(gateways.size() - 1)));
         int gateway_x = (int)(1024.0 * Math.cos(angle));
         int gateway_z = (int)(1024.0 * Math.sin(angle));
-        return new BPos(gateway_x, 0, gateway_z);
+        return new CPos(gateway_x >> 4, gateway_z >> 4);
     }
 
     public boolean filterEnd() {
-        BPos gateway = firstGateway(structureSeed);
-        RPos gatewayRegionPos = gateway.toRegionPos(20 << 4);
+        CPos gateway = firstGateway(structureSeed);
+        RPos gatewayRegionPos = gateway.toBlockPos().toRegionPos(20 << 4); //toRegionPos is only accurate with BPos
         RPos[][] regions = new RPos[3][3];
 
         for (int x = -1; x <= 1; x++) {
@@ -60,14 +133,16 @@ public class StructureFilter {
         for (RPos[] rowOfRegions : regions) {
             for (RPos region : rowOfRegions) {
                 CPos ecLocation = ec.getInRegion(structureSeed, region.getX(), region.getZ(), chunkRand);
-                double ecDistance = ecLocation.toBlockPos().distanceTo(gateway, DistanceMetric.EUCLIDEAN_SQ);
-                if ((ecDistance < MAX_DIST) && filterEndBiomes(ecLocation)) {
+                double ecDistance = ecLocation.distanceTo(gateway, DistanceMetric.EUCLIDEAN_SQ);
+                if ((ecDistance < MAX_DIST) && citySpawnsWithShip(ecLocation)) {
                     ecLocations.add(ecLocation);
                 }
             }
         }
 
-        if (ecLocations.isEmpty()) return false;
+        if (ecLocations.isEmpty()) {
+            return false;
+        }
 
         for (CPos cityEntry : ecLocations) {
             if (filterEndLoot(cityEntry)) {
@@ -78,18 +153,19 @@ public class StructureFilter {
         return false;
     }
 
-    public boolean filterEndBiomes(CPos ecLocation) {
-        if (!ec.canSpawn(ecLocation.getX(), ecLocation.getZ(), source)) return false;
-        if (!ec.canGenerate(ecLocation.getX(), ecLocation.getZ(), terrainGen)) return false;
+    public boolean citySpawnsWithShip(CPos ecLocation) {
+        if (!ec.canSpawn(ecLocation, endBiomeSource) || !ec.canGenerate(ecLocation, endTerrainGen)) {
+            return false;
+        }
 
-        ecGen.generate(terrainGen, ecLocation, chunkRand);
+        ecGen.generate(endTerrainGen, ecLocation, chunkRand);
 
         return ecGen.hasShip();
     }
 
     public boolean filterEndLoot(CPos ecLocation) {
         boolean foundGoodSword = false;
-        ecGen.generate(terrainGen, ecLocation, chunkRand);
+        ecGen.generate(endTerrainGen, ecLocation, chunkRand);
         List<ChestContent> loot = ec.getLoot(structureSeed, ecGen, chunkRand, false);
 
         lootLoop:
